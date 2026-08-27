9d42788ac930365a401a847f27737f50d2b611ab
Sujay Yatham <sujayy639@gmail.com>
2026-08-27 21:24:21 +0530

    Implement Kth largest element finder using PriorityQueue

---
+++ Arrays/PriorityQueue/KthLargestElementInAnArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int num : nums){
+            pq.add(num);
+        }
+        for(int i=0;i<nums.length-k;i++){
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
