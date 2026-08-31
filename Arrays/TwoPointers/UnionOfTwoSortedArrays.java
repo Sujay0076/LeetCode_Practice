3e1d459ac5aca01949061ba1f2e7b6636b6ce38b
Sujay Yatham <sujayy639@gmail.com>
2026-08-31 14:22:34 +0530

    Add unionOfArrays method to merge two sorted arrays
    Implement union of two sorted arrays using two pointers.

--- Arrays/TwoPointers/UnionOfTwoSortedArrays.java
+++ Arrays/TwoPointers/UnionOfTwoSortedArrays.java
@@ -1,1 +1,44 @@
+import java.util.*;
+public class UnionOfTwoSortedArrays {
+    public static void main(String[] args) {
+       int[] arr1 = {1, 2, 4, 5};
+       int[] arr2 = {2, 3, 5, 6};
+       List<Integer> union = unionOfArrays(arr1,arr2);
+       for(int num : union){
+        System.out.print(num+" ");
+       }
+    }
+    public static List<Integer> unionOfArrays(int[] a1,int[] a2){
+        int i=0;
+        int j =0;
+        List<Integer> list = new ArrayList<>();
+        while(i < a1.length && j < a2.length){
+            if(a1[i] < a2[j]){
+                list.add(a1[i]);
+                i++;
+               
+            }
+            else if(a1[i] == a2[j]){
+                list.add(a1[i]);
+                i++;
+                j++;
+            }
+            else{
+                list.add(a2[j]);
+                j++;
+            }
+        }
+        while(i < a1.length){
+            list.add(a1[i]);
+            i++;
+        }
+        while(j < a2.length){
+            list.add(a2[j]);
+            j++;
+        }
+        return list;
+    }
+
+    
+}
 
