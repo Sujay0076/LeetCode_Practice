a5e4b96f5a081854a84b353b8072c96824617b8f
Sujay Yatham <sujayy639@gmail.com>
2026-08-31 14:23:11 +0530

    Add CheckIfArraySorted class with isSorted method
    Implement isSorted method to check if an array is sorted.

--- Arrays/CheckIfArraySorted.java
+++ Arrays/CheckIfArraySorted.java
@@ -1,1 +1,16 @@
+public class CheckIfArraySorted {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,8};
+        // int[] arr = {2,4,1,6,7};
+        System.out.println(isSorted(arr));
+    }
+    public static boolean isSorted(int[] arr){
+        for(int i=1;i<arr.length;i++){
+            if(!(arr[i] >= arr[i-1])){
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
