566e8bfbf61551cf7e6f2cfaf21a272c2ed8a852
Sujay Yatham <sujayy639@gmail.com>
2026-09-01 22:23:04 +0530

    Implement matrix rotation in RotateImage class

--- Arrays/Matrix/RotateImage.java
+++ Arrays/Matrix/RotateImage.java
@@ -1,1 +1,30 @@
-
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swapForTranspose(matrix,i,j);
+            }
+        }
+        int left =0;
+        int right =n-1;
+        while(left < right){
+            for(int i=0;i<n;i++){
+                swapForRowReverse(matrix,i,left,right);
+            }
+            left++;
+            right--;
+        }
+        
+    }
+    public void swapForRowReverse(int[][]mat,int i,int left,int right){
+        int temp = mat[i][left];
+        mat[i][left] = mat[i][right];
+        mat[i][right] = temp;
+    }
+    public void swapForTranspose(int[][] mat,int i,int j){
+        int temp = mat[i][j];
+        mat[i][j] = mat[j][i];
+        mat[j][i] = temp;
+    }
+}
