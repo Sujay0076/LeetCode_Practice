c972589f13d56945ce1e6ea6be1dffcb7d6a645a
Sujay Yatham <sujayy639@gmail.com>
2026-09-02 17:37:23 +0530

    Add nextPermutation method to Solution class
    Implement next permutation algorithm for an array of integers.

--- Arrays/Greedy/NextPermutation.java
+++ Arrays/Greedy/NextPermutation.java
@@ -1,1 +1,37 @@
-
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int index =-1;
+        for(int i=nums.length-1;i>0;i--){
+            if(nums[i] > nums[i-1]){
+                index =i-1;
+                break;
+            }
+        }
+        if(index == -1){
+            reverse(nums,0,nums.length-1);
+            return;
+        }
+        for(int i=nums.length-1;i>index;i--){
+            if(nums[i] > nums[index]){
+                swap(nums,i,index);
+                break;
+            }
+        }
+        reverse(nums,index+1,nums.length-1);
+    }
+    
+    public void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public void reverse(int[] arr,int left ,int right){
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
