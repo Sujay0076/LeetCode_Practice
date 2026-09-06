48e7b48fa2843da7276b4cc40e777c67edfa1b43
Sujay Yatham <sujayy639@gmail.com>
2026-09-06 13:18:49 +0530

    Implement method to generate Pascal's Triangle row

--- Arrays/PascalTriangle/PascalTriangleII.java
+++ Arrays/PascalTriangle/PascalTriangleII.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        long ans =1;
+        List<Integer> res = new ArrayList<>();
+        res.add(1);
+        for(int j=1;j<=rowIndex;j++){
+            ans = ans*(rowIndex-j+1);
+            ans = ans/j;
+            res.add((int)ans);
+        }
+        return res;
+    }
+}
