4d127dc0c0c8fd6339c88d2b30b85b3d0d3b5d0f
Sujay Yatham <sujayy639@gmail.com>
2026-09-08 10:37:02 +0530

    Add subarray sum equals zero functionality
    Implemented a method to find the longest subarray with a sum of zero.

--- Arrays/HashMap/SubarraySumEquals0.java
+++ Arrays/HashMap/SubarraySumEquals0.java
@@ -1,1 +1,26 @@
-
+import java.util.*;
+class SubarraySumEquals0{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for(int i=0;i<arr.length;i++){
+      arr[i] = sc.nextInt();
+    }
+    subarraySumEquals0(arr);
+  }
+  public static void subarraySumEquals0(int[] arr){
+    Map<Integer,Integer> map = new HashMap<>();
+    map.put(0,-1);
+    int sum =0;
+    int maxLen =-1;
+    for(int i=0;i<arr.length;i++){
+      sum += arr[i];
+      if(map.containsKey(sum)){
+        maxLen = Math.max(maxLen,i-map.get(sum));
+      }
+      else{
+        map.put(sum,i);
+      }
+    }
+  }
