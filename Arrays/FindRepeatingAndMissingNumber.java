ec80fd4e35fcc24bda71f412f92266eb23ae53e5
Sujay Yatham <sujayy639@gmail.com>
2026-09-10 16:32:01 +0530

    Implement method to find repeating and missing numbers

--- Arrays/FindRepeatingAndMissingNumber.java
+++ Arrays/FindRepeatingAndMissingNumber.java
@@ -1,1 +1,21 @@
-
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,3,4};
+        int n = arr.length;
+        long s = (long)(n * (n+1))/2;
+        long sn =0;
+        long s2 = (long)(n * (n+1)* (2 * n+1))/6;
+        long s2n =0;
+        for(int i=0;i<n;i++){
+            sn += (long)arr[i];
+            s2n += (long)arr[i] * (long)arr[i];
+        }
+        long val1 = s - sn;
+        long val2 = s2 - s2n;
+        val2 = val2/val1;
+        long x = (val1+val2)/2;
+        long y = x- val1;
+        System.out.println(x+" "+y); 
+    }
+}
