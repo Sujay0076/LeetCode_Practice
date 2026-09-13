83acfae1ad0dbba3151a31a6555005ad315d3609
Sujay Yatham <sujayy639@gmail.com>
2026-09-13 12:26:01 +0530

    Implement binary search for first and last occurrence

--- Arrays/BinarySearch/FirstAndLastOccurrence.java
+++ Arrays/BinarySearch/FirstAndLastOccurrence.java
@@ -1,1 +1,36 @@
-
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first=-1;
+        int last =-1;
+        int low =0;
+        int high = nums.length-1;
+        int mid =0;
+        while(low <= high){
+            mid = low+(high-low)/2;
+            if(nums[mid] >= target){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        if(low < nums.length && nums[low] == target){
+            first = low;
+        }
+        low =0;
+        high = nums.length-1;
+        while(low <= high){
+            mid = low+(high-low)/2;
+            if(nums[mid] <= target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        if(high >= 0 && nums[high] == target){
+            last = high;
+        }
+        return new int[]{first,last};
+    }
+}
