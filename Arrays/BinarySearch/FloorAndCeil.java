0a0516f5bd9935b52c67c79cd65dd905a2e13e9b
Sujay Yatham <sujayy639@gmail.com>
2026-09-13 12:28:00 +0530

    Implement floor and ceil functions in FloorAndCeil.java
    Fixed syntax errors and improved binary search logic for floor and ceil calculations.

--- Arrays/BinarySearch/FloorAndCeil.java
+++ Arrays/BinarySearch/FloorAndCeil.java
@@ -1,1 +1,38 @@
-
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        int low =0;
+        int high = arr.length-1;
+        int mid =0;
+        int x =sc.nextInt();
+        int floor =0;
+        int ceil =0;
+        while(low <= high){
+            mid = low+(high-low)/2;
+            if(arr[mid] <= x){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        if (high < 0) {
+            floor = -1;
+        } else {
+            floor = arr[high];
+        }
+        
+        if (low >= arr.length) {
+            ceil = -1;
+        } else {
+            ceil = arr[low];
+        }
+        System.out.println(floor+" "+ceil);
+        
+    }
+}
