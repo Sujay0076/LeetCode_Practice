8b5bfccba55c78bc9fadae7f58f946824ae9a88b
Sujay Yatham <sujayy639@gmail.com>
2026-09-15 16:07:22 +0530

    Implement search in rotated sorted array II

--- Arrays/BinarySearch/SearchInRotatedSortedArrayII.java
+++ Arrays/BinarySearch/SearchInRotatedSortedArrayII.java
@@ -1,1 +1,35 @@
+class Solution {
+    public boolean search(int[] arr, int target) {
+        int low =0;
+        int high = arr.length-1;
+        int mid =0;
 
+        while(low <= high){
+            mid = low+(high-low)/2;
+            if(arr[mid] == target){
+                return true;
+            }
+            else if((arr[low] == arr[mid])&&(arr[mid] == arr[high])){
+                low++;
+                high--;
+            }
+            else if(arr[low] <= arr[mid]){
+                if(arr[low] <= target && arr[mid] >= target){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else{
+                if(arr[mid] <= target && arr[high] >= target){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}
