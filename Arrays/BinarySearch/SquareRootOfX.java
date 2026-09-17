162f2059234154069245a2ead5bd006c97138dcd
Sujay Yatham <sujayy639@gmail.com>
2026-09-17 16:14:21 +0530

    Implement binary search for square root calculation

--- Arrays/BinarySearch/SquareRootOfX.java
+++ Arrays/BinarySearch/SquareRootOfX.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public int mySqrt(int x) {
+        int low =1;
+        int high = x;
+        int mid = 0;
+        while(low <= high){
+            mid = low+(high-low)/2;
+            if((long)mid * (long)mid == x){
+                return mid;
+            }
+            else if((long)mid*(long)mid > x){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return high;
+    }
+}
