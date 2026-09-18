9fda75d2f778570c9212bfe81cf682165c084c33
Sujay Yatham <sujayy639@gmail.com>
2026-09-18 09:49:12 +0530

    Implement Nth root calculation using binary search

--- Arrays/BinarySearch/NthRootOfANumber.java
+++ Arrays/BinarySearch/NthRootOfANumber.java
@@ -1,1 +1,25 @@
-
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int low = 1;
+        int high=n;
+        int mid =0;
+        while(low <= high){
+            mid = low+(high-low)/2;
+            if((Math.pow(mid,m)) == n){
+                System.out.println(mid);
+                return;
+            }
+            else if(Math.pow(mid,m) > n){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        System.out.println(-1);
+    }
+}
