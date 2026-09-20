5f1479765db450e2edb80ac66a885ca4f9e01996
Sujay Yatham <sujayy639@gmail.com>
2026-09-20 13:06:33 +0530

    Implement binary search for Kth missing number

--- Arrays/BinarySearch/KthMissingNumber.java
+++ Arrays/BinarySearch/KthMissingNumber.java
@@ -1,1 +1,20 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int low =0;
+        int high = arr.length-1;
+        int mid =0;
+        int missing =0;
 
+        while(low <= high){
+            mid = low+(high-low)/2;
+            missing = arr[mid]-(mid+1);
+            if(missing < k){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return high+1+k;
+    }
+}
