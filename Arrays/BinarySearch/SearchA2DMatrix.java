a6bcad630f3bab16311d379ee6a3c0b280f586cf
Sujay Yatham <sujayy639@gmail.com>
2026-09-24 14:41:36 +0530

    Add searchMatrix method for 2D matrix search
    Implement binary search to find target in 2D matrix.

--- Arrays/BinarySearch/SearchA2DMatrix.java
+++ Arrays/BinarySearch/SearchA2DMatrix.java
@@ -1,1 +1,43 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int low =0;
+        int high =matrix.length-1;
 
+        int mid =0;
+
+        while(low <= high){
+            mid = low+(high-low)/2;
+            if(matrix[mid][0] > target){
+                high = mid-1;
+            }
+            else if(matrix[mid][0] == target){
+                return true;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+
+        int row = low-1;
+        if(row < 0){
+            return false;
+        }
+        low =0;
+        high = matrix[0].length-1;
+
+        while(low <= high){
+            mid = low+(high-low)/2;
+            if(matrix[row][mid] > target){
+                high = mid-1;
+            }
+            else if(matrix[row][mid] == target){
+                return true;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return false;
+        
+    }
+}
