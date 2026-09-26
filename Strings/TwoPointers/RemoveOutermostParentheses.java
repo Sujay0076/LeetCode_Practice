4254ff0a99169c0501a5c37224bbd4488b756efb
Sujay Yatham <sujayy639@gmail.com>
2026-09-26 11:40:41 +0530

    Add method to remove outermost parentheses
    Implement a method to remove the outermost parentheses from a valid parentheses string.

--- Strings/TwoPointers/RemoveOutermostParentheses.java
+++ Strings/TwoPointers/RemoveOutermostParentheses.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
+        int left =0;
+        int depth =0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '('){
+                depth++;
+            }
+            else{
+                depth--;
+                if(depth == 0){
+                    sb.append(s.substring(left+1,i));
+                    left = i+1;
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
